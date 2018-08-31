ed6f884c7522e30d89c937a4145676a20831a294
Alexander Ku <kualexcw28@gmail.com>
2018-08-31 08:21:22 -0600

    Add chapter9 folder

---
+++ chapter9/Exercise2.java
@@ -0,0 +1,9 @@
+public class Exercise2 {
+
+    public static void main (String [] args) {
+        double x = 4.0;
+        double result = 3 * (x * x) - (8 * x) + 4;
+
+        System.out.println("Equation = " + result);
+    }
+}
