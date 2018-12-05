c00b5afa92b1300c732f856297148f2bcec741fb
Alexander Ku <kualexcw28@gmail.com>
2018-12-05 07:58:44 -0700

    Work on phone book

--- part7/chapter66/Exercise1.java
+++ part7/chapter66/Exercise1.java
@@ -6,40 +6,48 @@ public class Exercise1
   {
     PhoneBook pb = new PhoneBook();  
     Scanner scan = new Scanner(System.in);
-    System.out.print("Name? ");
-    String name = scan.nextLine();
+    System.out.print("Last Name? ");
+    String lastName = scan.nextLine();
 
-    while (!name.equals("quit")) {
+    while (!lastName.equals("quit")) {
         
         // search for "Violet Smith"
-        if (!name.equals("quit")) {
-            PhoneEntry entry = pb.search( name ); 
+      if (!lastName.equals("quit")) {
+        System.out.print("First Name? ");
+        String firstName = scan.nextLine();
+
+        PhoneEntry entry = pb.search( lastName ); 
             
-            if ( entry != null)
-                System.out.println( entry.getName() + ": " + entry.getPhone() );
-            else {
-                System.out.println("Name not found" );
-            }
-            System.out.print("Name? ");
-            name = scan.nextLine();
+        if ( entry != null && firstName.length() > 0) {
+          System.out.println( "The number is: " + entry.getPhone() );
+        } else {
+          System.out.println("Name not found" );
         }
+        System.out.print("Last Name? ");
+        lastName = scan.nextLine();
+      }
     }
     System.out.println("good-by");
+    
   }
 }
 
 class PhoneEntry
 {
-  private String name;    // name of a person
+  private String firstName;
+  private String lastName;
+  private String name = firstName + " " + lastName;
   private String phone;   // their phone number
 
-  public PhoneEntry( String n, String p )
+  public PhoneEntry( String lN, String fN, String p )
   {
-    name = n; phone = p;
+    lastName = lN; firstName = fN; phone = p;
   }
   
-  public String getName()  {return name;}
+  public String getName()  {return lastName;}
+  public String getFirstName() {return firstName;}
   public String getPhone() {return phone;}
+  
 }
 
 class PhoneBook
@@ -48,13 +56,15 @@ class PhoneBook
 
   public PhoneBook()    // constructor
   {
-    phoneBook = new PhoneEntry[ 5 ] ;
+    phoneBook = new PhoneEntry[ 7 ] ;
 
-    phoneBook[0] = new PhoneEntry( "James Barclay", "(418) 665-1223" );
-    phoneBook[1] = new PhoneEntry( "Grace Dunbar", "(860) 399-3044" );
-    //phoneBook[2] = new PhoneEntry( "Paul Kratides", "(815) 439-9271" );
-    phoneBook[3] = new PhoneEntry( "Violet Smith", "(312) 223-1937" );
-    phoneBook[4] = new PhoneEntry( "John Wood", "(913) 883-2874" );
+    phoneBook[0] = new PhoneEntry( "Barclay", "James", "(418) 665-1223" );
+    phoneBook[1] = new PhoneEntry( "Dunbar", "Grace", "(860) 399-3044" );
+    phoneBook[2] = new PhoneEntry( "Kratides", "Paul", "(815) 439-9271" );
+    phoneBook[3] = new PhoneEntry( "Smith", "Violet", "(312) 223-1937" );
+    phoneBook[4] = new PhoneEntry( "Wood", "John", "(913) 883-2874" );
+    phoneBook[5] = new PhoneEntry("Smith", "John", "(812) 339-4916");
+    phoneBook[6] = new PhoneEntry("Smith", "Willoughby", "(312) 992-8761");
 
   }
 
@@ -68,6 +78,18 @@ class PhoneBook
 
     return null;
   }
+
+  // public void findCell() {
+  //   for (int i = 0; i < phoneBook.length; i++) {
+  //     if (phoneBook[i] == null) {
+  //       phoneBook[i] ;
+  //     } 
+  //   }
+  // }
+
+  // public void deleteEntry() {
+  //   phoneBook[findCell()] = null;
+  // }
 }
 
 
